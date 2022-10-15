ea38ad3e903b1dd9d55415b6baaba16e1aa579a8
Felipe Guilherme <111253219+Hyperdarks@users.noreply.github.com>
2022-10-15 02:30:59 -0300

    Add files via upload

---
+++ C/idades.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+void limpar_entrada()
+{
+    char c;
+    while ((c = getchar()) != '\n' && c != EOF) {}
+}
+
+void ler_texto(char *buffer, int length)
+{
+    fgets(buffer, length, stdin);
+    strtok(buffer, "\n");
+}
+
+
+int main()
+{
+    char nome1[50], nome2[50];
+    int idade1, idade2;
+    double media;
+    printf("\nDados da primeira pessoa: \n");
+    printf("Nome: ");
+    gets(nome1);
+    printf("Idade: ");
+    scanf("%d", &idade1);
+
+    printf("\nDados da segunda pessoa: \n");
+    printf("Nome: ");
+    limpar_entrada();
+    gets(nome2);
+    printf("Idade: ");
+    scanf("%d", &idade2);
+
+    media = (double) (idade1 + idade2) / 2;
+
+    printf("A idade media de %s e %s eh de %.1lf anos", nome1, nome2, media);
+
+
+
+
+
+
+
+    return 0;
+}
